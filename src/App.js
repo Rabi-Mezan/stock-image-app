6593c26ee0241b2e9dd7236b81e207face45c830
Rabi-Mezan <ravimezan@gmail.com>
2023-05-08 16:00:28 +0600

    add photo via link done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Navbar from './components/Navbar';
+import UploadForm from './components/uploadform';
+
 
 
 
@@ -14,6 +17,23 @@ const photos = [
 ]
 
 function App() {
+
+  const [isCollaspe, setIsCollaspe] = useState(false);
+  const [input, setInput] = useState();
+  const [items, setItems] = useState(photos)
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value)
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setItems([input, ...items])
+  }
+
+
+  const toogle = () => setIsCollaspe(!isCollaspe)
+
+
   return (
     <>
       {/* navbar */}
@@ -21,12 +41,22 @@ function App() {
 
       <div className="App">
         {/* title */}
-        <h1 className='bg-warning py-4 display-6 fs-2'>Stock Photo Gallery</h1>
+        <h1 className='bg-warning py-4 display-6 fs-2'>Stock Photo Gallery </h1>
+        <button onClick={toogle} className='btn btn-success '>{isCollaspe ? "Close" : "+Add Photo"}</button>
+
+        <div className='my-5'>
+
+          <UploadForm
+            isCollaspe={isCollaspe}
+            onChange={handleOnChange}
+            onSubmit={handleOnSubmit}
+          />
+        </div>
 
         {/* gallery home image cards */}
         <div className='row m-5 d-flex justify-content-center align-items-center'>
           {
-            photos.map((photo) => <Card src={photo} />)
+            items.map((photo, index) => <Card key={index} src={photo} />)
           }
         </div>
       </div>
